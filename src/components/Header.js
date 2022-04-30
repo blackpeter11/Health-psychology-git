0e4a0b06ad704ceafdcd2fb30ca36ac832cb7ad1
blackpeter11 <273676794@qq.com>
2022-05-01 04:38:22 +0800

    first git

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from './button'
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Task Tacker</h1>
+      <Button />
+    </header>
+  )
+}
+
+export default Header
